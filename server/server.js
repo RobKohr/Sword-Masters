fb183010484e12001e6ea079dd87f63c4c610ebc
Rob Kohr <spam@robkohr.com>
2017-09-27 23:09:12 -0400

    On lesson 5

--- server/server.js
+++ server/server.js
@@ -2,11 +2,17 @@ var io = require('socket.io')(process.env.PORT || 3000);
 
 console.log('server started');
 
+var playerCount = 0;
 io.on('connection', function(socket){
     console.log('client connected, broadcasting spawn');
     socket.broadcast.emit('spawn');
-
+    playerCount++;
+    for(var i = 0; i < playerCount; i++){
+        socket.emit('spawn');
+        console.log('sending span to new player')
+    }
     socket.on('move', function(data){
        console.log('client moved');
     });
+
 })
